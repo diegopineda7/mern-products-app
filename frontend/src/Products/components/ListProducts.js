4e1cf7a281f3cbd6b481376bbe619d3fc3108d72
Diego Pineda <pineda.diego798@gmail.com>
2020-09-22 22:43:38 -0500

    Get products from  API

--- frontend/src/Products/components/ListProducts.js
+++ frontend/src/Products/components/ListProducts.js
@@ -1,8 +1,21 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
+import { getProducts } from '../services'
 import Loading from './Loading'
 
 const ListProducts = () => {
   const [isLoading, setIsLoading] = useState(true)
+  const [products, setProducts] = useState([])
+
+  useEffect(() => {
+    const loadProducts = async () => {
+      const response = await getProducts()
+      if (response.status === 200)
+        setProducts(response.data.products)
+    }
+
+    loadProducts()
+  }, [])
+
 
   return (
     <div className='columns is-centered'>
